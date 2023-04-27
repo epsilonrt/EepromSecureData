7657e009e01d4a39600b5c030236c268a2757c7d
epsilonrt <epsilonrt@gmail.com>
2023-04-28 01:04:29 +0200

    ci: expand the example

--- examples/EepromSecureDataSimple/EepromSecureDataSimple.ino
+++ examples/EepromSecureDataSimple/EepromSecureDataSimple.ino
@@ -1,38 +1,89 @@
 //
-// EepromSecureData Simple Example 
+// EepromSecureData Simple Example
 //
-// This example ...
 #include <Arduino.h>
 #include <EepromSecureData.h>
 
-struct Dummy {
+// My dummy class
+// This class must be triavially copyable, i.e. it must have a trivial copy constructor
+struct MyClass {
   int a;
   int b;
   int c;
-  Dummy (int a, int b, int c) : a (a), b (b), c (c) {}
-  Dummy() : Dummy (0, 0, 0) {}
-  Dummy (const Dummy &) = default;
-  bool  operator == (const Dummy &rhs) const {
+  // Constructor with parameters
+  MyClass (int a, int b, int c) : a (a), b (b), c (c) {}
+  // Default constructor
+  MyClass() : MyClass (0, 0, 0) {}
+  // Copy constructor
+  MyClass (const MyClass &) = default; // This is required
+  // Comparison operator
+  bool  operator == (const MyClass &rhs) const {
     return a == rhs.a && b == rhs.b && c == rhs.c;
   }
-  bool  operator != (const Dummy &rhs) const {
+  // Inequality operator
+  bool  operator != (const MyClass &rhs) const {
     return ! (*this == rhs);
   }
-
+  // Print method
+  void print (const char *prefix = "", Print & out = Serial) const {
+    out.print (prefix);
+    out.print (a);
+    out.write (' ');
+    out.print (b);
+    out.write (' ');
+    out.println (c);
+  }
 };
 
-const Dummy dv1 (1, 2, 3);
-EepromSecureData<Dummy> sd1;
+const MyClass dv1 (1, 2, 3);
+EepromSecureData<MyClass> sd1;
 
 void setup() {
 
   delay (2000);
   Serial.begin (115200);
+  Serial.println ("\n[EepromSecureDataSimple] Example");
 
-  sd1.load(dv1);
-  
+  #if defined(ESP32) || defined(ESP8266)
+  EEPROM.begin (4096);
+  #endif
+
+  // Load from EEPROM
+  if (sd1.load (dv1)) {
+    Serial.println ("Loaded from EEPROM");
+  }
+  else {
+    Serial.println ("Loaded default");
+  }
+  sd1.data().print ("sd1: ");
+
+  MyClass d2 (sd1);
+  MyClass d3 (4,5,6);
+  // Compare
+  if (d2 == sd1) {
+    Serial.println ("d2 == sd1");
+  }
+  else {
+    Serial.println ("d2 != sd1");
+  }
+  if (d3 == sd1) {
+    Serial.println ("d3 == sd1");
+  }
+  else {
+    Serial.println ("d3 != sd1");
+  }
+
+  sd1 = d3;
+  // Save to EEPROM
+  if (sd1.save()) {
+    Serial.println ("Saved to EEPROM");
+  }
+  else {
+    Serial.println ("Save failed");
+  }
+
 }
 
 void loop() {
-
+  // nothing to do here
 }
